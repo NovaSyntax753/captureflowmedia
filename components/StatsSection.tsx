c6920574368deb439513407c2e2291480ee6fc71
VaibhavTalkhande <vaibhavtalkhande41@gmail.com>
2025-09-05 00:43:52 +0530

    stats fixed

--- components/StatsSection.tsx
+++ components/StatsSection.tsx
@@ -7,12 +7,12 @@ export default function StatsSection() {
   ];
 
   return (
-    <section className="w-full bg-black text-white py-12">
+    <section className="w-full text-black py-12">
       <div className="  grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-10 text-center border-y border-gray-700 py-8">
         {stats.map((stat, index) => (
           <div key={index}>
             <h3 className="text-2xl font-bold text-green-500">{stat.number}</h3>
-            <p className="mt-2 text-sm leading-relaxed">{stat.text}</p>
+            <p className="mt-2 text-sm font-bold leading-relaxed">{stat.text}</p>
           </div>
         ))}
       </div>
